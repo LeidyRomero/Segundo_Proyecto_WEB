dd2ebb5d754eb8bf7a4b8d8e3ad86fb84299dd73
Pablo Suarez <pa.suarezm@uniandes.edu.co>
2019-09-23 10:49:09 -0500

    build(?)

---
+++ app-back/front/build/precache-manifest.d6e2c0fb57623708e08e810336f4afb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6071ae0e4ec4f7eb052d4da7c6001b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48e6d8205ba58111323f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f311417f8c562f9cb1cb",
+    "url": "/static/css/main.e8bcccd4.chunk.css"
+  },
+  {
+    "revision": "48e6d8205ba58111323f",
+    "url": "/static/js/2.3c9366e6.chunk.js"
+  },
+  {
+    "revision": "f311417f8c562f9cb1cb",
+    "url": "/static/js/main.85eec2cf.chunk.js"
+  },
+  {
+    "revision": "9ba826fd182ef2cb5489",
+    "url": "/static/js/runtime~main.cef8c144.js"
+  }
+]);
